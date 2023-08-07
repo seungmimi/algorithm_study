a1721cdcda7eaeb2a6b8417f44fef432744d09c6
seungmimi <138555225+seungmimi@users.noreply.github.com>
2023-08-07 17:43:29 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = 0;
+    if(n % 7 == 0) {
+        answer = n/7;
+        return answer; 
+
+    }else{
+        answer = Math.floor(n/7)+1;
+        return answer; 
+    }
+}
