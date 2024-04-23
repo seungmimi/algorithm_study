f2306455df015b58fddf0342ecb0de75b822b55d
seungmimi <138555225+seungmimi@users.noreply.github.com>
2024-04-23 09:15:51 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,9 @@
+function solution(s1, s2) {
+    var answer = 0;
+    for(let i = 0; i < s1.length; i++){
+        if(s2.indexOf(s1[i]) !== -1){
+            answer++
+        }
+    }
+    return answer;
+}
